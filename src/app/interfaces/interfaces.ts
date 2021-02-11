fed64fc6d4674fd2dddae647d8c64bc35438cad1
Kevinava <kevinava1996@gmail.com>
2021-02-11 11:48:46 +0100

    changes

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -12,13 +12,11 @@ export interface MenuOpts {
 
 /**
  * Interfaz para las categorías.
- * @param {string} id - ID de la categoría.
  * @param {string} slug - Ruta de la categoría.
  * @param {string} name - Nombre de la categoría.
  * @param {string} image - Icono a mostrar en la categoría.
  */
 export interface Category {
-    id: string;
     slug: string;
     name: string;
     image: string;
@@ -26,7 +24,6 @@ export interface Category {
 
 /**
  * Interfaz para las series.
- * @param {string} id - ID de la serie.
  * @param {string} slug - Ruta de la serie.
  * @param {string} title - Título de la serie.
  * @param {number} year - Año de estreno de la serie.
@@ -35,9 +32,9 @@ export interface Category {
  * @param {number} episodes - Número de episodios totales que tiene la serie.
  * @param {object} categories - Array con las diferentes categorías de la serie.
  * @param {object} images - Array con las diferentes imagenes de la serie.
+ * @param {object} comments - Array con los ObjectId de los comentarios
  */
 export interface Serie {
-    id: string;
     slug: string;
     title: string;
     year: number;
@@ -46,22 +43,21 @@ export interface Serie {
     episodes: number;
     categories: object;
     images: object;
+    comments: object;
 }
 
 /**
  * Interfaz para los comentarios.
- * @param {number} id - El ID definido para dicho comentario.
- * @param {string} slug - La ruta de la serie puntuada.
  * @param {string} email - El email del usuario que puntua la serie.
- * @param {number} puntuacion - Puntuación obtenida a dicha serie por el usuario.
- * @param {string} comentario - Comentario opcional por dicho usuario.
- * @param {string} fecha - Fecha de la publicación de dicho comentario.
+ * @param {number} rating - Puntuación obtenida a dicha serie por el usuario.
+ * @param {string} message - Comentario opcional por dicho usuario.
+ * @param {any} serie - ObjectId de la serie en la que se publica el comentario.
+ * @param {Date} date - Fecha de la publicación de dicho comentario.
  */
 export interface Comentaries {
-    id: number;
-    slug: string;
     email: string;
-    puntuacion: number;
-    comentario: string;
-    fecha: string;
+    rating: number;
+    message: string;
+    serie: any;
+    date: Date;
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,6 @@ import { Category, Serie, Comentaries, MenuOpts } from '../interfaces/interfaces
 export class DataService {
 
   APIURL = 'http://localhost:3000/api/';
-  // APIURL = 'https://json-peliseries.herokuapp.com/';
 
   // Http Options
   httpOptions = {
@@ -20,64 +19,31 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  /* GET */
-
-  /**
-   * Método para obtener todas las páginas del menu.
-   * @return
-   */
   getMenuOpts() {
     return this.http.get<MenuOpts[]>('/assets/data/menu.json')
   }
 
-  /**
-   * Método para obtener todas las categorías.
-   * @return
-   */
   getCategories() {
-    return this.http.get<Category[]>(this.APIURL+'categories');
+    return this.http.get<Category[]>(`${this.APIURL}categories`);
   }
   
-  /**
-   * 
-   * @param slug Slug de la serie a buscar los comentarios
-   */
-  getComentarios(slug) {
-    // return this.http.get<Comentaries[]>(this.APIURL+'series/'+serieSlug+'/comments');
-    return this.http.get<Comentaries[]>(this.APIURL+'series/'+ slug +'/comments');
+  getComentarios(slug: string) {
+    return this.http.get<Comentaries[]>(`${this.APIURL}series/${slug}/comments`);
   }
   
-  /**
-   * Método para obtener todas las series.
-   * @return
-   */
   getSeries() {
-    return this.http.get<Serie[]>(this.APIURL+'series');
+    return this.http.get<Serie[]>(`${this.APIURL}series`);
   }
 
-
-  getSerie(slug) {
-    return this.http.get<Serie[]>(this.APIURL+'series/' + slug);
+  getSerie(slug: string) {
+    return this.http.get<Serie[]>(`${this.APIURL}series/${slug}`);
   }
   
-  /**
-   * Método para obtener los datos de una categoría determinada.
-   * @param {string} slugCat - Slug de la categoría a obtener.
-   * @return
-   */
-  getCategory(slugCat) {
-    return this.http.get<Category[]>(this.APIURL+'categories' + '/' + slugCat);
+  getCategory(slug: string) {
+    return this.http.get<Category[]>(`${this.APIURL}categories/${slug}`);
   }
 
-
-  /* POST */
-
-  /**
-   * Método para crear un comentario y almacenarlo.
-   * @param {object} comentario - Todos los datos a guardar sobre el comentario nuevo.
-   * @return
-   */
-  postComentario(slug,comentario) {
-    return this.http.post<Comentaries>(this.APIURL + 'series/' + slug + '/comments', JSON.stringify(comentario), this.httpOptions);
+  postComentario(slug: string, comentario: Comentaries) {
+    return this.http.post<Comentaries>(`${this.APIURL}series/${slug}/comments`, JSON.stringify(comentario), this.httpOptions);
   }
 }
